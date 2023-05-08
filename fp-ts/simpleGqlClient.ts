793892569bc25458e5527b9f67129c6461abf5c8
Nathan Argetsinger <nargetdev@gmail.com>
2023-05-08 11:38:02 -0600

    removing fp-ts for this branch

--- fp-ts/simpleGqlClient.ts
+++
@@ -1,6 +0,0 @@
-import { Client, cacheExchange, fetchExchange } from '@urql/core';
-
-const client = new Client({
-    url: 'http://127.0.0.1:8080/graphql',
-    exchanges: [cacheExchange, fetchExchange],
-});
