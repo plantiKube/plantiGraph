f3c2740bf476f87a6990e1f901c9edbe9d74509c
Nathan Argetsinger <nargetdev@gmail.com>
2023-05-11 17:37:53 -0600

    enumerate 18 sites per germination Tray

--- graphql-request/initScripts/instantiateNursery.ts
+++ graphql-request/initScripts/instantiateNursery.ts
@@ -4,40 +4,65 @@ import {
     CreateGermTrayMutationVariables, GerminationTray, GerminationTrayRef,
     InstantiateNurseryDocument,
     InstantiateNurseryMutation, InstantiateNurseryMutationVariables,
-    SignalType,
+    SignalType, SiteRef,
     WheresChuckDocument,
     WheresChuckQuery,
     WheresChuckQueryVariables
 } from "../../operations";
 import {GraphQLClient} from "graphql-request";
 import {pipe} from "fp-ts/function";
+import {makeBy, replicate} from "fp-ts/ReadonlyNonEmptyArray";
+import * as A from 'fp-ts/lib/Array'
+import {number} from "fp-ts";
+import * as assert from "assert";
 
+
+function ExpandSiteRef(i: number): SiteRef {
+    console.log(i)
+    return {
+        signal: {
+            signalDate: "2023-05-10T23:13:56Z",
+            signalType: SignalType.Transfer
+        }
+    }
+}
 
 export function ExpandGerminationBool(signal: boolean): GerminationTrayRef {
+    // expand out the sites
+
+    const foo = [1, 2, 3]
+    const bar = ['a', 'b', 'c']
+    const zipped = pipe(foo, A.zip(bar))
+    console.log(zipped)
+
+    const desiredSites: Array<SiteRef> = A.map<number, SiteRef>(ExpandSiteRef)([1, 2, 3])
+
+    console.log(desiredSites)
+
+    const desiredSitesPipe: Array<SiteRef> = pipe(
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        A.map<number, SiteRef>(() => {
+            return {
+                signal: {
+                    signalDate: "2023-05-10T23:13:56Z",
+                    signalType: SignalType.Transfer
+                }
+            }
+        })
+        // A.map(ExpandSiteRef)
+    )
+
+    console.log(desiredSitesPipe)
+
     return signal ?
         {
-            sites:
-                [
-                    {
-                        signal: {
-                            signalDate: "2023-05-10T23:13:56Z",
-                            signalType: SignalType.Germinate
-                        }
-                    }
-                ]
+            sites: desiredSites
         } :
         {
-            sites:
-                [
-                    {
-                        signal: {
-                            signalDate: "2023-05-10T23:13:56Z",
-                            signalType: SignalType.Transfer
-                        }
-                    }
-                ]
+            sites: desiredSitesPipe
         }
 }
+
 export const instantiateNurseryEffect = () => {
 
     const client = new GraphQLClient("http://localhost:8080/graphql", {fetch});
@@ -47,7 +72,7 @@ export const instantiateNurseryEffect = () => {
     const desiredGerminationTrays: Array<GerminationTrayRef> = pipe(
         [false, true, true, false, false, false, false],
         (bools) => bools.map(ExpandGerminationBool),
-        )
+    )
 
     // console.log(desiredGerminationTrays)
 
