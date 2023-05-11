9ac90697b1dd11bef1e4c527999da4bcb44c7c50
Nathan Argetsinger <nargetdev@gmail.com>
2023-05-11 16:23:40 -0600

    gonna try removing xid

--- graphql-request/initScripts/instantiateNursery.test.ts
+++ graphql-request/initScripts/instantiateNursery.test.ts
@@ -1,7 +1,13 @@
-import {instantiateNurseryEffect} from "./instantiateNursery";
+import {ExpandGerminationBool, instantiateNurseryEffect} from "./instantiateNursery";
+import {GerminationTray, GerminationTrayRef} from "../../operations";
 
 describe('instantiateNursery runs', function () {
     it('runs', function () {
         instantiateNurseryEffect()
+    });
+
+    it ('expands the Germination tray bool', function () {
+        const germTray: GerminationTrayRef = ExpandGerminationBool(true)
+        console.log(germTray)
     });
 });
--- graphql-request/initScripts/instantiateNursery.ts
+++ graphql-request/initScripts/instantiateNursery.ts
@@ -1,7 +1,7 @@
 import {
     CreateGermTrayDocument,
     CreateGermTrayMutation,
-    CreateGermTrayMutationVariables, GerminationTrayRef,
+    CreateGermTrayMutationVariables, GerminationTray, GerminationTrayRef,
     InstantiateNurseryDocument,
     InstantiateNurseryMutation, InstantiateNurseryMutationVariables,
     SignalType,
@@ -10,39 +10,50 @@ import {
     WheresChuckQueryVariables
 } from "../../operations";
 import {GraphQLClient} from "graphql-request";
+import {pipe} from "fp-ts/function";
 
 
+export function ExpandGerminationBool(signal: boolean): GerminationTrayRef {
+    return signal ?
+        {
+            sites:
+                [
+                    {
+                        signal: {
+                            signalDate: "2023-05-10T23:13:56Z",
+                            signalType: SignalType.Germinate
+                        }
+                    }
+                ]
+        } :
+        {
+            sites:
+                [
+                    {
+                        signal: null
+                    }
+                ]
+        }
+}
 export const instantiateNurseryEffect = () => {
 
     const client = new GraphQLClient("http://localhost:8080/graphql", {fetch});
 
     // Ok we're going to build our germTraysArray
 
-    const desiredGerminationTrays: Array<GerminationTrayRef> = new Array<GerminationTrayRef>();
+    const desiredGerminationTrays: Array<GerminationTrayRef> = pipe(
+        [false, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false],
+        (bools) => bools.map(ExpandGerminationBool),
+        )
 
+    // console.log(desiredGerminationTrays)
 
     client.request<InstantiateNurseryMutation, InstantiateNurseryMutationVariables>
     (
         InstantiateNurseryDocument,
         {
-            nurseryXid: "abcdefg",
-            germTraysArray: [
-                {
-                    xid: "flie223",
-                    sites:
-                    [
-                        {
-                            xid: "flie2",
-                            signal: {
-                                xid: "s00001",
-                                signalDate: "2023-05-10T23:13:56Z",
-                                signalType: SignalType.Germinate
-                            }
-                        }
-                    ]
-
-                }
-            ]
+            nurseryXid: "abazz",
+            germTraysArray: desiredGerminationTrays
         }
     ).then((data) => {
         console.log(data)
