966f318defdb98cc177c74ca90958a9c2883bae9
Nathan Argetsinger <nargetdev@gmail.com>
2023-06-16 13:45:41 -0600

    start flattened site array mutation. get those signals in there

---
+++ graphql-request/initScripts/instantiateModuleMap/instantiateFlattenedSiteArray/instantiateFlattenedSiteArray.test.ts
@@ -0,0 +1,7 @@
+import {instantiateFlattenedSiteArray} from "./instantiateFlattenedSiteArray";
+
+describe('instantiateFlattenedArray mutation runs', function () {
+    it('caninstantiate a flattened site map', function () {
+        instantiateFlattenedSiteArray()
+    });
+});
--- graphql-request/initScripts/instantiateModuleMap/instantiateFlattenedSiteArray/instantiateFlattenedSiteArray.ts
+++ graphql-request/initScripts/instantiateModuleMap/instantiateFlattenedSiteArray/instantiateFlattenedSiteArray.ts
@@ -1,8 +1,28 @@
 import {GraphQLClient} from "graphql-request";
+import {
+    InstantiateFlattenedSiteArrayDocument,
+    InstantiateFlattenedSiteArrayMutation,
+    InstantiateFlattenedSiteArrayMutationVariables, SignalType
+} from "../../../../operations";
 
 export const instantiateFlattenedSiteArray = () => {
 
     const client = new GraphQLClient("http://localhost:8080/graphql", {fetch});
 
-    // client.request<
+    client.request<InstantiateFlattenedSiteArrayMutation, InstantiateFlattenedSiteArrayMutationVariables>(InstantiateFlattenedSiteArrayDocument,
+        {
+            "sites": [
+                {
+                    "signal":{
+                        "signalType": SignalType.Germinate,
+                        "signalDate":"2023-06-16T19:07:09Z"
+                    }
+                }
+            ]
+        }
+        ).then((data) => {
+        console.log(data)
+        let oneFlattenedSiteID = data.addFlattenedSiteArray.flattenedSiteArray["0"].id;
+        console.log(oneFlattenedSiteID)
+    });
 }
